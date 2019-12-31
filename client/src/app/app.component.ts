18c6c02f5c980debfff54c15b6971406dd3b092e
MostafaSamer <mostafasamer1999@hotmail.com>
2020-01-01 01:38:49 +0200

    login & Register Design side menu in progress

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'client';
+  slider = [
+      {
+          'header': 'Account',
+          'data': ['login', 'profile']
+      },
+      {
+          'header': 'Courses',
+          'data': ['Recomeded Courses', 'Course of the day']
+      },
+      {
+          'header': 'Application',
+          'data': ['Rate us', 'Send Feedback & Rebort', 'Ads removal', 'Privacy Policy']
+      }
+  ]
 }
